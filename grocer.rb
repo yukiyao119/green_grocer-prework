aa84db027cac0d816163a06ce6c8fb9093b6c23c
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-06 22:19:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,6 +37,12 @@ def apply_coupons(cart, coupons)
   # binding.pry
 end
 
+# cart {
+#   "PEANUT BUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
+#   "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+#   "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
+# }
+
 def apply_clearance(cart)
   # code here
   
