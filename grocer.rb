0cf7306c1cea89fb76c169b2fe39aad5e4ab9987
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-06 22:11:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,50 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_cart = cart.uniq
+  result = {}
+  new_cart.collect do |item_hash|
+    item_quan = cart.count(item_hash)
+    item_hash.collect do |item, details|
+    result[item] = details
+    result[item][:count] = item_quan
+    # binding.pry
+    end
+  end
+  result
+  # binding.pry
 end
 
+# cart {
+#   "AVOCADO" => {:price => 3.00, :clearance => true, :count => 3}
+# }
+# coupons [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
+
 def apply_coupons(cart, coupons)
-  # code here
+  result = cart
+  coupons.each do |coupon|
+    item_name = coupon[:item]   # "AVOCADO"
+    if cart.keys.include?(item_name) && cart[item_name][:count] >= coupon[:num]
+      if result[item_name+" W/COUPON"].nil?
+        result[item_name+" W/COUPON"] = {
+          :price => (coupon[:cost] / coupo[:num]),
+          :clearance => cart[item_name][:clearance],
+          :count => coupon[:num]
+        }
+        result[item_name][:count] = result[item_name][:count] - coupon[:num]
+      else
+        result[item_name+" W/COUPON"][:count] += coupon[:num]
+      end 
+    end 
+  end 
+  result
+  # binding.pry
 end
+
+# result{
+#   "AVOCADO" => {:price => 3.00, :clearance => true, :count => 1},
+#   "AVOCADO W/COUPON" => {:price => 2.50, :clearance => true, :count => 2},
+# }
 
 def apply_clearance(cart)
   # code here
